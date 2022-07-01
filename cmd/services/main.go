2a19866dbbc5f47a9dcbbb433c7b88a71252924a
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2022-07-01 02:10:59 -0700

    Update main.go
    * Added function setting application environment
    * Updated code to use updated config settings

--- cmd/services/main.go
+++ cmd/services/main.go
@@ -3,18 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/sflewis2970/trivia-service/config"
 	"github.com/sflewis2970/trivia-service/routes"
 )
 
+func setCfgEnv() {
+	// Set hostname environment variable
+	os.Setenv(config.HOSTNAME, "")
+
+	// Set hostport environment variable
+	os.Setenv(config.HOSTPORT, ":8080")
+
+	// Set Datastore Server address or DNS environment variable
+	os.Setenv(config.DSNAME, "http://ds-service")
+	os.Setenv(config.DSPORT, ":9090")
+
+	// Response messages
+	os.Setenv(config.CONGRATS, "Congrats! That is correct")
+	os.Setenv(config.TRYAGAIN, "Nice try! Better luck on the next question")
+}
+
 func main() {
 	// Initialize logging
 	log.SetFlags(log.Ldate | log.Lshortfile)
 
-	// Create config object and load config data into memory
-	log.Print("Loading config data...")
-	cfg := config.GetConfig()
+	useCfgFile := os.Getenv("USECONFIGFILE")
+	if len(useCfgFile) == 0 {
+		setCfgEnv()
+	}
+
+	// Get config data
+	cfgData, getCfgDataErr := config.Get().GetData(config.UPDATE_CONFIG_DATA)
+	if getCfgDataErr != nil {
+		log.Fatal("Error getting config data: ", getCfgDataErr)
+	}
 
 	// Create App
 	rs := routes.CreateRoutingServer()
@@ -22,7 +46,7 @@ func main() {
 	// Start Server
 	log.Print("Web service server is ready...")
 
-	addr := cfg.GetConfigData().HostName + cfg.GetConfigData().HostPort
+	addr := cfgData.HostName + cfgData.HostPort
 	log.Print("The address used by the service is: ", addr)
 	log.Fatal(http.ListenAndServe(addr, rs.Router))
 }
