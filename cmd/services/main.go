56855986741b30323ea9aa6d48f7c1a262f875a6
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2022-07-05 07:08:04 -0700

    Update main.go
    * Removed setting environment variables. This action is done during build process using docker.

--- cmd/services/main.go
+++ cmd/services/main.go
@@ -3,36 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/sflewis2970/trivia-service/config"
 	"github.com/sflewis2970/trivia-service/routes"
 )
 
-func setCfgEnv() {
-	// Set hostname environment variable
-	os.Setenv(config.HOSTNAME, "")
-
-	// Set hostport environment variable
-	os.Setenv(config.HOSTPORT, ":8080")
-
-	// Set Datastore Server address or DNS environment variable
-	os.Setenv(config.DSNAME, "http://ds-service")
-	os.Setenv(config.DSPORT, ":9090")
-
-	// Response messages
-	os.Setenv(config.CONGRATS, "Congrats! That is correct")
-	os.Setenv(config.TRYAGAIN, "Nice try! Better luck on the next question")
-}
-
 func main() {
 	// Initialize logging
 	log.SetFlags(log.Ldate | log.Lshortfile)
-
-	useCfgFile := os.Getenv("USECONFIGFILE")
-	if len(useCfgFile) == 0 {
-		setCfgEnv()
-	}
 
 	// Get config data
 	cfgData, getCfgDataErr := config.Get().GetData(config.UPDATE_CONFIG_DATA)
