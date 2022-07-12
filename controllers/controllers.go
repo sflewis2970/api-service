c2f9ed4890ff80f9b70bb23d99ee539d5e9fbf12
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2022-07-12 10:06:34 -0700

    Update controllers.go
    * Moved constants into separate package
    * refactored code for more clarity

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -3,74 +3,40 @@ package controllers
 import (
 	"log"
 
-	"github.com/sflewis2970/trivia-service/api"
+	apis "github.com/sflewis2970/trivia-service/api"
 	"github.com/sflewis2970/trivia-service/config"
-	"github.com/sflewis2970/trivia-service/datastore"
+	"github.com/sflewis2970/trivia-service/models"
 )
 
-const (
-	DASH             string = "-"
-	COMMA            string = ","
-	SPACE            string = " "
-	ONE_SET          int    = 1
-	FIND_ERROR       int    = -1
-	EMPTY_QUESTIONID int    = 0
-	EMPTY_QUESTION   int    = 0
-	EMPTY_ANSWER     int    = 0
-	EMPTY_CATEGORY   int    = 0
-	EMPTY_CHOICES    int    = 0
-	EMPTY_TIMESTAMP  int    = 0
-	EMPTY_WARNING    int    = 0
-)
-
-// Unexported package functions
-// containsDuplicates checks the slice for any duplicate items
-func containsDuplicates(items []api.TriviaResponse) bool {
-	// Initialize the map for usage
-	itemsMap := make(map[string]int)
-
-	// Since maps uses unique keys, use the string value of answer to be the key
-	for idx, item := range items {
-		itemsMap[item.Answer] = idx + 1
-	}
-
-	// If the size of the map is the same size of the slice, then there are no duplicates
-	if len(itemsMap) != len(items) {
-		return true
-	}
-
-	// Otherwise return false
-	return false
+// Controller struct definition
+type Controller struct {
+	cfgData     *config.ConfigData
+	publicAPI   *apis.API
+	triviaModel *models.Model
 }
 
-// Global controller component
-var ctrlr *controllerComponent
-
-type controllerComponent struct {
-	cfgData *config.ConfigData
-	ds      *datastore.QuestionDataStore
-}
-
-func (c *controllerComponent) initializeDS() *datastore.QuestionDataStore {
-	// Create datastore component
-	return datastore.Initialize()
-}
+// Packge controller object
+var controller *Controller
 
 // Export functions
-func Initialize() {
-	if ctrlr == nil {
+func New() {
+	if controller == nil {
 		// Create controller component
-		ctrlr = new(controllerComponent)
+		log.Print("Creating controller object...")
+		controller = new(Controller)
 
 		// Load config data
 		var getCfgDataErr error
-		ctrlr.cfgData, getCfgDataErr = config.Get().GetData()
+		controller.cfgData, getCfgDataErr = config.Get().GetData()
 		if getCfgDataErr != nil {
 			log.Print("Error getting config data: ", getCfgDataErr)
 			return
 		}
 
-		// Initialize Datastore
-		ctrlr.ds = ctrlr.initializeDS()
+		// Create trivia model
+		controller.triviaModel = models.New()
+
+		// Create trivia api
+		controller.publicAPI = apis.New()
 	}
 }
