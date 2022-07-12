5b22af8b2667e61fa05d42751a23d0cfec4dfa34
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2022-07-12 10:01:27 -0700

    Update main.go
    * Added time to logger
    * Added cors to routing system

--- cmd/services/main.go
+++ cmd/services/main.go
@@ -4,13 +4,14 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/rs/cors"
 	"github.com/sflewis2970/trivia-service/config"
 	"github.com/sflewis2970/trivia-service/routes"
 )
 
 func main() {
 	// Initialize logging
-	log.SetFlags(log.Ldate | log.Lshortfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
 	// Get config data
 	cfgData, getCfgDataErr := config.Get().GetData(config.UPDATE_CONFIG_DATA)
@@ -19,12 +20,25 @@ func main() {
 	}
 
 	// Create App
-	rs := routes.CreateRoutingServer()
+	rs := routes.New()
+
+	// setup Cors
+	log.Print("Setting up CORS...")
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{http.MethodPost, http.MethodGet},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: false,
+	})
+	corsHandler := cors.Handler(rs.Router)
+
+	// Server Address info
+	addr := cfgData.HostName + cfgData.HostPort
+	log.Print("The address used by the service is: ", addr)
 
 	// Start Server
 	log.Print("Web service server is ready...")
 
-	addr := cfgData.HostName + cfgData.HostPort
-	log.Print("The address used by the service is: ", addr)
-	log.Fatal(http.ListenAndServe(addr, rs.Router))
+	// Listen and Serve
+	log.Fatal(http.ListenAndServe(addr, corsHandler))
 }
